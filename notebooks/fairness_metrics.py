91d841be0edde6a227cd7d1eb2d65c9ced2ebabb
Kristiana Karshelieva <kris.krsh02@gmail.com>
2025-11-17 22:43:21 -0600

    fairness metrics implemented

---
+++ notebooks/fairness_metrics.py
@@ -0,0 +1,18 @@
+from sklearn.metrics import confusion_matrix
+
+def demographic_parity(data, majority, minority):
+    probability_1 = len(data[(data["Ethnicity"] == majority) & (data["PredictedDiagnosis"])]) / len(data[(data["Ethnicity"] == majority)])
+    probability_2 = len(data[(data["Ethnicity"] == minority) & (data["PredictedDiagnosis"])]) / len(data[(data["Ethnicity"] == minority)])
+    return abs(probability_1 - probability_2)
+
+def equal_opportunity(data, majority, minority):
+    conf_matrix_1 = confusion_matrix(data[(data["Ethnicity"] == majority)]["TrueDiagnosis"], data[(data["Ethnicity"] == majority)]["PredictedDiagnosis"]).ravel().tolist()
+    conf_matrix_2 = confusion_matrix(data[(data["Ethnicity"] == minority)]["TrueDiagnosis"], data[(data["Ethnicity"] == minority)]["PredictedDiagnosis"]).ravel().tolist()
+    
+    return abs(conf_matrix_1[3]/(conf_matrix_1[3] + conf_matrix_1[1]) - conf_matrix_2[3]/(conf_matrix_2[3] + conf_matrix_2[1]))
+
+
+def disparate_impact(data, majority, minority):
+    probability_1 = len(data[(data["Ethnicity"] == majority) & (data["PredictedDiagnosis"])]) / len(data[(data["Ethnicity"] == majority)])
+    probability_2 = len(data[(data["Ethnicity"] == minority) & (data["PredictedDiagnosis"])]) / len(data[(data["Ethnicity"] == minority)])
+    return probability_1 / probability_2
