1572beab7d4b9288cf176df6e836e1bc52a3c78b
Hunter37594 <hunter37594@gmail.com>
2025-12-07 17:48:33 -0600

    Implement equalized odds and refactor equal opportunity to reuse its output

--- notebooks/fairness_metrics.py
+++ notebooks/fairness_metrics.py
@@ -6,11 +6,21 @@ def demographic_parity(data, majority, minority):
     return abs(probability_1 - probability_2)
 
 def equal_opportunity(data, majority, minority):
-    conf_matrix_1 = confusion_matrix(data[(data["Ethnicity"] == majority)]["TrueDiagnosis"], data[(data["Ethnicity"] == majority)]["PredictedDiagnosis"]).ravel().tolist()
-    conf_matrix_2 = confusion_matrix(data[(data["Ethnicity"] == minority)]["TrueDiagnosis"], data[(data["Ethnicity"] == minority)]["PredictedDiagnosis"]).ravel().tolist()
-    
-    return abs(conf_matrix_1[3]/(conf_matrix_1[3] + conf_matrix_1[1]) - conf_matrix_2[3]/(conf_matrix_2[3] + conf_matrix_2[1]))
+    return equalized_odds(data, majority, minority)[0]
 
+def equalized_odds(data, majority, minority):
+    tn_maj, fp_maj, fn_maj, tp_maj = confusion_matrix(data[(data["Ethnicity"] == majority)]["TrueDiagnosis"], data[(data["Ethnicity"] == majority)]["PredictedDiagnosis"], labels=[0,1]).ravel().tolist()
+    tn_min, fp_min, fn_min, tp_min = confusion_matrix(data[(data["Ethnicity"] == minority)]["TrueDiagnosis"], data[(data["Ethnicity"] == minority)]["PredictedDiagnosis"], labels=[0,1]).ravel().tolist()
+    
+    tpr_maj = tp_maj/(tp_maj + fn_maj)
+    tpr_min = tp_min/(tp_min + fn_min)
+    fpr_maj = fp_maj/(fp_maj + tn_maj)
+    fpr_min = fp_min/(fp_min + tn_min)
+
+    tpr_diff = abs(tpr_maj - tpr_min)
+    fpr_diff = abs(fpr_maj - fpr_min)
+
+    return [tpr_diff, fpr_diff]
 
 def disparate_impact(data, majority, minority):
     probability_1 = len(data[(data["Ethnicity"] == majority) & (data["PredictedDiagnosis"])]) / len(data[(data["Ethnicity"] == majority)])
